b58457063f9187f58c541213bfd97d4216bc644e
JonasBechler <jonasbechler@gmail.com>
2023-03-02 11:22:22 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 
-
 import './App.css'
 import './SWK_React_Template/src/styles/App.css'
 
@@ -10,15 +9,16 @@ import Main from './SWK_React_Template/src/components_intern/Main/Main';
 function App() {
 	const name = "Handyticket";
 	const icon = require("./Handyticket.png");
+	const mainview = require("./Handyticket_MainView.png");
+
 	const config = require( "./config.json");
-	
 	
 	config.port = config.handyticket.port
 	config.port_react = config.handyticket.port_react
 
 	return (
 
-		<Main name={name} icon={icon} config={config}></Main>
+		<Main name={name} icon={icon} config={config} mainview={mainview}></Main>
 
 	);
 }
